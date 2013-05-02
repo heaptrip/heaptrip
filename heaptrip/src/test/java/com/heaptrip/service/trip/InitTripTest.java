c30dc0c12a836bfebd201c498dac7df32d5a2923
evgeny-konovalov <konovalov.inbox@gmail.com>
2013-05-02 16:50:22 +0400

    add tests for getNearTableItem and getNearTableItemByPeriod

--- heaptrip/src/test/java/com/heaptrip/service/trip/InitTripTest.java
+++ heaptrip/src/test/java/com/heaptrip/service/trip/InitTripTest.java
@@ -1,6 +1,8 @@
 package com.heaptrip.service.trip;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -14,13 +16,15 @@ import com.heaptrip.domain.entity.ContentCategory;
 import com.heaptrip.domain.entity.ContentOwner;
 import com.heaptrip.domain.entity.ContentRegion;
 import com.heaptrip.domain.entity.MultiLangText;
+import com.heaptrip.domain.entity.trip.TableItem;
 import com.heaptrip.domain.entity.trip.Trip;
 import com.heaptrip.domain.service.trip.TripService;
+import com.heaptrip.util.RandomUtils;
 
 @ContextConfiguration("classpath*:META-INF/spring/test-context.xml")
 public class InitTripTest extends AbstractTestNGSpringContextTests {
 
-	static int TRIPS_COUNT = 10;
+	static long TRIPS_COUNT = 10;
 
 	static String OWNER_ID = "1";
 
@@ -29,7 +33,31 @@ public class InitTripTest extends AbstractTestNGSpringContextTests {
 	@Autowired
 	private TripService tripService;
 
-	private void init() {
+	private TableItem[] getRandomTable() {
+		int tableSize = RandomUtils.getRandomInt(1, 10);
+		TableItem[] table = new TableItem[tableSize];
+		for (int j = 0; j < tableSize; j++) {
+			TableItem item = new TableItem();
+
+			Calendar startBegin = Calendar.getInstance();
+			startBegin.set(2013, 0, 1);
+			Calendar startEnd = Calendar.getInstance();
+			startEnd.set(2013, 8, 1);
+			Calendar dateEnd = Calendar.getInstance();
+			dateEnd.set(2013, 11, 1);
+
+			Date begin = RandomUtils.getRandomDate(startBegin.getTime(), startEnd.getTime());
+			Date end = RandomUtils.getRandomDate(begin, dateEnd.getTime());
+
+			item.setBegin(begin);
+			item.setEnd(end);
+
+			table[j] = item;
+		}
+		return table;
+	}
+
+	private void initTrips() {
 		trips = new ArrayList<>();
 		Locale locale = Locale.ENGLISH;
 
@@ -45,6 +73,9 @@ public class InitTripTest extends AbstractTestNGSpringContextTests {
 			trip.setDescription(new MultiLangText("my description", locale));
 			trip.setSummary(new MultiLangText("my summary", locale));
 			trip.setLangs(new String[] { locale.getLanguage() });
+
+			trip.setTable(getRandomTable());
+
 			trips.add(trip);
 		}
 	}
@@ -52,7 +83,7 @@ public class InitTripTest extends AbstractTestNGSpringContextTests {
 	@BeforeTest()
 	public void beforeTest() throws Exception {
 		this.springTestContextPrepareTestInstance();
-		init();
+		initTrips();
 		for (Trip trip : trips) {
 			tripService.saveTrip(trip);
 		}
--- heaptrip/src/test/java/com/heaptrip/service/trip/TripServiceTest.java
+++ heaptrip/src/test/java/com/heaptrip/service/trip/TripServiceTest.java
@@ -1,5 +1,7 @@
 package com.heaptrip.service.trip;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +11,13 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.heaptrip.domain.entity.trip.TableItem;
 import com.heaptrip.domain.entity.trip.Trip;
 import com.heaptrip.domain.service.ContentSortEnum;
+import com.heaptrip.domain.service.SearchPeriod;
 import com.heaptrip.domain.service.trip.TripCriteria;
 import com.heaptrip.domain.service.trip.TripService;
+import com.heaptrip.util.RandomUtils;
 
 @ContextConfiguration("classpath*:META-INF/spring/test-context.xml")
 public class TripServiceTest extends AbstractTestNGSpringContextTests {
@@ -23,24 +28,88 @@ public class TripServiceTest extends AbstractTestNGSpringContextTests {
 	@DataProvider(name = "tripCriteria")
 	public Object[][] createTripCriteria() {
 		TripCriteria tripCriteria = new TripCriteria();
-		tripCriteria.setCategoryIds(new String[] { "1.2" });
-		tripCriteria.setSkip(5L);
-		tripCriteria.setLimit(5L);
-		tripCriteria.setSort(ContentSortEnum.CREATED);
 		tripCriteria.setOwnerId(InitTripTest.OWNER_ID);
+		tripCriteria.setCategoryIds(new String[] { "1.2" });
+		tripCriteria.setSkip(0L);
+		tripCriteria.setLimit(InitTripTest.TRIPS_COUNT);
+		tripCriteria.setSort(ContentSortEnum.CREATED);
+		Calendar begin = Calendar.getInstance();
+		begin.set(2013, 0, 1);
+		Calendar end = Calendar.getInstance();
+		end.set(2013, 11, 1);
+		SearchPeriod period = new SearchPeriod(begin.getTime(), end.getTime());
+		tripCriteria.setPeriod(period);
 		return new Object[][] { new Object[] { tripCriteria } };
+	}
+
+	@DataProvider(name = "tripWithTable")
+	private Object[][] getTripWithTable() {
+		int tableSize = RandomUtils.getRandomInt(1, 10);
+		TableItem[] table = new TableItem[tableSize];
+		for (int j = 0; j < tableSize; j++) {
+			TableItem item = new TableItem();
+
+			Calendar startBegin = Calendar.getInstance();
+			startBegin.set(2013, 0, 1);
+			Calendar startEnd = Calendar.getInstance();
+			startEnd.set(2013, 8, 1);
+			Calendar dateEnd = Calendar.getInstance();
+			dateEnd.set(2013, 11, 1);
+
+			Date begin = RandomUtils.getRandomDate(startBegin.getTime(), startEnd.getTime());
+			Date end = RandomUtils.getRandomDate(begin, dateEnd.getTime());
+
+			item.setBegin(begin);
+			item.setEnd(end);
+
+			table[j] = item;
+		}
+
+		Trip trip = new Trip();
+		trip.setTable(table);
+
+		return new Object[][] { new Object[] { trip } };
 	}
 
 	@Test(dataProvider = "tripCriteria", enabled = false)
 	public void getTripsByCriteria(TripCriteria tripCriteria) {
 		List<Trip> trips = tripService.getTripsByCriteria(tripCriteria);
-		Assert.assertEquals(trips.size(), 5L);
+		Assert.assertEquals(trips.size(), InitTripTest.TRIPS_COUNT);
 	}
 
 	@Test(dataProvider = "tripCriteria", enabled = false)
 	public void getTripsCountByCriteria(TripCriteria tripCriteria) {
 		Long count = tripService.getTripsCountByCriteria(tripCriteria);
 		Assert.assertNotNull(count);
-		Assert.assertTrue(count.equals(5L));
+		Assert.assertTrue(count.equals(InitTripTest.TRIPS_COUNT / 2L));
+	}
+
+	@Test(dataProvider = "tripWithTable", enabled = false)
+	public void getNearTableItem(Trip trip) {
+		TableItem item = tripService.getNearTableItem(trip);
+		Assert.assertNotNull(item);
+		for (TableItem ti : trip.getTable()) {
+			Assert.assertTrue(item.equals(ti) || item.getBegin().before(ti.getBegin()));
+		}
+	}
+
+	@Test(dataProvider = "tripWithTable", enabled = false)
+	public void getNearTableItemByPeriod(Trip trip) {
+		Calendar begin = Calendar.getInstance();
+		begin.set(2013, 0, 1);
+		Calendar end = Calendar.getInstance();
+		end.set(2013, 6, 1);
+		SearchPeriod period = new SearchPeriod(begin.getTime(), end.getTime());
+
+		TableItem item = tripService.getNearTableItemByPeriod(trip, period);
+
+		if (item != null) {
+			Assert.assertTrue(item.getBegin().after(period.getDateBegin())
+					&& item.getBegin().before(period.getDateEnd()));
+
+			for (TableItem ti : trip.getTable()) {
+				Assert.assertTrue(item.equals(ti) || item.getBegin().before(ti.getBegin()));
+			}
+		}
 	}
 }
