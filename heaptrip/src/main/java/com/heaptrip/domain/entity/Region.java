7bf646e51a90a14aa0368a9f87a67734d4d1d619
evgeny-konovalov <konovalov.inbox@gmail.com>
2013-05-28 15:42:07 +0400

    fix region entities

--- heaptrip/src/main/java/com/heaptrip/domain/entity/Region.java
+++ heaptrip/src/main/java/com/heaptrip/domain/entity/Region.java
@@ -1,11 +1,22 @@
 package com.heaptrip.domain.entity;
 
+/**
+ * 
+ * Region
+ * 
+ */
 public class Region extends ContentRegion {
+
 	public static final String COLLECTION_NAME = "regions";
 
+	// id of the parent
 	private String parent;
 
+	// id list of all parents
 	private String[] ancestors;
+
+	// type of region
+	private RegionEnum type;
 
 	public String getParent() {
 		return parent;
@@ -23,8 +34,11 @@ public class Region extends ContentRegion {
 		this.ancestors = ancestors;
 	}
 
-	public static String getCollectionName() {
-		return COLLECTION_NAME;
+	public RegionEnum getType() {
+		return type;
 	}
 
+	public void setType(RegionEnum type) {
+		this.type = type;
+	}
 }
---
+++ heaptrip/src/main/java/com/heaptrip/domain/entity/RegionEnum.java
@@ -0,0 +1,10 @@
+package com.heaptrip.domain.entity;
+
+/**
+ * 
+ * Enumeration type region
+ * 
+ */
+public enum RegionEnum {
+	COUNTRY, AREA, CITY
+}
