c27bbf10397bee24c67802916c9891995f259f09
evgeny-konovalov <konovalov.inbox@gmail.com>
2013-05-28 10:53:01 +0400

    OPEN - task 4: add get the categories of trip for a given locale https://github.com/heaptrip/heaptrip/issues/issue/4

--- heaptrip/src/main/java/com/heaptrip/repository/CategoryRepositoryImpl.java
+++ heaptrip/src/main/java/com/heaptrip/repository/CategoryRepositoryImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.heaptrip.domain.entity.Category;
 import com.heaptrip.domain.repository.CategoryRepository;
 import com.heaptrip.domain.repository.MongoContext;
+import com.heaptrip.util.LanguageUtils;
 import com.heaptrip.util.collection.IteratorConverter;
 
 @Service(CategoryRepository.SERVICE_NAME)
@@ -35,13 +36,15 @@ public class CategoryRepositoryImpl implements CategoryRepository {
 	@Override
 	public Category findById(String Id) {
 		MongoCollection coll = mongoContext.getCollection(Category.COLLECTION_NAME);
-		return coll.findOne("{ _id : #}", Id).as(Category.class);
+		return coll.findOne("{_id: #}", Id).as(Category.class);
 	}
 
 	@Override
 	public List<Category> findAll(Locale locale) {
 		MongoCollection coll = mongoContext.getCollection(Category.COLLECTION_NAME);
-		Iterable<Category> iter = coll.find().as(Category.class);
+		String lang = LanguageUtils.getLanguageByLocale(locale);
+		String fields = String.format("{'name.%s': 1, parent: 1, ancestors: 1}", lang);
+		Iterable<Category> iter = coll.find().projection(fields).as(Category.class);
 		return IteratorConverter.copyIterator(iter.iterator());
 	}
 
