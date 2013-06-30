b0cfb2af3595ec99605803cc7e74b4b33c7c6df0
Evgeny Konovalov <konovalov.inbox@gmail.com>
2013-06-30 13:41:53 +0400

    fix call to method of static java.text.DateFormat

--- heaptrip/src/main/java/com/heaptrip/util/SlugUtils.java
+++ heaptrip/src/main/java/com/heaptrip/util/SlugUtils.java
@@ -2,6 +2,7 @@ package com.heaptrip.util;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -9,13 +10,14 @@ public class SlugUtils {
 
 	private static SecureRandom random = new SecureRandom();
 
-	private static SimpleDateFormat formatter = new SimpleDateFormat("Y.M.d.H.m.s");
+	private static final String SLUG_DATE_PATTERN = "Y.M.d.H.m.s";
 
 	public static String generateSlug() {
 		return new BigInteger(24, random).toString(32);
 	}
 
 	public static String getFullSlugBySlug(String slug) {
+		final DateFormat formatter = new SimpleDateFormat(SLUG_DATE_PATTERN);
 		return formatter.format(new Date()) + ":" + slug;
 	}
 
