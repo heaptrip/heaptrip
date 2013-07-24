b9a611be013e0384fc5a12594bd334129908efec
dikma <kolmogorov.dikma@gmail.com>
2013-07-24 11:33:29 +0400

    profile

---
+++ heaptrip/src/main/java/com/heaptrip/domain/entity/user/Knowledge.java
@@ -0,0 +1,64 @@
+package com.heaptrip.domain.entity.user;
+
+import java.util.Date;
+
+import com.heaptrip.domain.entity.BaseObject;
+
+public class Knowledge extends BaseObject {
+	
+	// begin date
+	private Date begin;
+
+	// end date
+	private Date end;
+
+	private String specialist;
+
+	private String location;
+	
+	private String document;
+
+	public Date getBegin() {
+		return begin;
+	}
+	
+	public Knowledge() {
+		super();
+	}
+
+	public void setBegin(Date begin) {
+		this.begin = begin;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	public String getSpecialist() {
+		return specialist;
+	}
+
+	public void setSpecialist(String specialist) {
+		this.specialist = specialist;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}	
+}
