10d8df058540552dc96809caddfcff5d85be6d8d
evgeny-konovalov <konovalov.inbox@gmail.com>
2013-10-18 16:09:26 +0400

    refactor by idea

--- heaptrip/src/main/java/com/heaptrip/domain/entity/BaseObject.java
+++ heaptrip/src/main/java/com/heaptrip/domain/entity/BaseObject.java
@@ -5,52 +5,47 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.jongo.marshall.jackson.oid.Id;
 
 /**
- * 
  * Base object for all entities that are stored in the collection and have a
  * unique identifier
- * 
  */
 public abstract class BaseObject {
 
-	@Id
-	protected String id;
+    @Id
+    protected String id;
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (obj == this) {
-			return true;
-		}
-		if (obj.getClass() != this.getClass()) {
-			return false;
-		}
-		BaseObject bo = (BaseObject) obj;
-		if (getId() == null || bo.getId() == null) {
-			return false;
-		}
-		return new EqualsBuilder().append(getId(), bo.getId()).isEquals();
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+        BaseObject bo = (BaseObject) obj;
+        return getId() != null && bo.getId() != null && new EqualsBuilder().append(getId(), bo.getId()).isEquals();
+    }
 
-	@Override
-	public int hashCode() {
-		if (getId() == null) {
-			return super.hashCode();
-		}
-		return new HashCodeBuilder().append(getId()).toHashCode();
-	}
+    @Override
+    public int hashCode() {
+        if (getId() == null) {
+            return super.hashCode();
+        }
+        return new HashCodeBuilder().append(getId()).toHashCode();
+    }
 
-	@Override
-	public String toString() {
-		return this.getClass().getName() + "[id=" + getId() + "]";
-	}
+    @Override
+    public String toString() {
+        return this.getClass().getName() + "[id=" + getId() + "]";
+    }
 }
