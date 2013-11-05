d3d1dcb6865a146cea3539a7e1efe676a0fb62eb
Evgeny Konovalov <konovalov.inbox@gmail.com>
2013-11-05 19:15:37 +0400

    add service for upload image

--- heaptrip/src/main/java/com/heaptrip/domain/service/image/ImageService.java
+++ heaptrip/src/main/java/com/heaptrip/domain/service/image/ImageService.java
@@ -1,44 +1,46 @@
 package com.heaptrip.domain.service.image;
 
+import com.heaptrip.domain.entity.image.ImageEnum;
+
 import java.io.IOException;
 import java.io.InputStream;
 
-import com.heaptrip.domain.entity.image.ImageEnum;
-
 /**
- * 
  * Image service
- * 
  */
 public interface ImageService {
 
-	/**
-	 * Save image
-	 * 
-	 * @param fileName
-	 *            file name
-	 * @param imageType
-	 *            typr of image
-	 * @param is
-	 *            input stream
-	 * @return image id
-	 */
-	public String saveImage(String fileName, ImageEnum imageType, InputStream is) throws IOException;
+    /**
+     * Save image
+     *
+     * @param fileName file name
+     * @param is       input stream
+     * @return image id
+     */
+    public String saveImage(String fileName, InputStream is) throws IOException;
 
-	/**
-	 * Get image
-	 * 
-	 * @param imageId
-	 *            image id
-	 * @return input stream
-	 */
-	public InputStream getImage(String imageId);
+    /**
+     * Save image
+     *
+     * @param fileName  file name
+     * @param imageType type of image
+     * @param is        input stream
+     * @return image id
+     */
+    public String saveImage(String fileName, ImageEnum imageType, InputStream is) throws IOException;
 
-	/**
-	 * Remove image
-	 * 
-	 * @param imageId
-	 *            image id
-	 */
-	public void removeImage(String imageId);
+    /**
+     * Get image
+     *
+     * @param imageId image id
+     * @return input stream
+     */
+    public InputStream getImage(String imageId);
+
+    /**
+     * Remove image
+     *
+     * @param imageId image id
+     */
+    public void removeImage(String imageId);
 }
