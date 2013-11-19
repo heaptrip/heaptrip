d617ddf9eb95a2545e3bee7867c9989ca7f28fab
Pavel Ogurtsov <pavel.ogurtsov@heaptrip.com>
2013-11-19 15:55:21 +0400

    edit marshrut

--- heaptrip/src/main/webapp/js/map.js
+++ heaptrip/src/main/webapp/js/map.js
@@ -25,6 +25,27 @@
 	});
 })(jQuery);
 
+var geocoder;
+var map;
+var poly;
+var infoWindow;
+var polyOptions = {
+    fillColor: '#00ff00',
+    fillOpacity: 1,
+    strokeWeight: 3,
+    clickable: true,
+    editable: true,
+    zIndex: 1
+}
+var flightPlanCoordinates = null;
+
+flightPlanCoordinates = [
+    new google.maps.LatLng(59.92749568088279, 30.3662109375),
+    new google.maps.LatLng(59.65109171169264, 31.080322265625),
+    new google.maps.LatLng(59.74532608213611,31.849365234375),
+    new google.maps.LatLng(59.92199002450385, 32.34375)
+];
+
 $(window).bind("onPageReady", function(e, paramsJson) {
 	initializeMap();
 	$('#google_map_canvas').onShow(function() {
@@ -36,19 +57,44 @@ var initializeMap = function() {
     var mapCanvas = $('#google_map_canvas');
     mapCanvas.width('98%');
     mapCanvas.height(300);
+
+    geocoder = new google.maps.Geocoder();
     var mapOptions = {
-        center : new google.maps.LatLng(59.9, 30.4),
-        zoom : 8,
-
-        mapTypeId : google.maps.MapTypeId.ROADMAP
+        zoom: 8,
+        center: new google.maps.LatLng(59.9, 30.4),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(mapCanvas[0], mapOptions);
-
-
-    new google.maps.Marker({
-        position: new google.maps.LatLng(59.9, 30.4),
-        map: map,
-        title:"Пример маркера"
-
-    });
+    map = new google.maps.Map(mapCanvas[0],mapOptions);
+    newPoly(flightPlanCoordinates);
+    google.maps.event.addListener(map, 'click', addLatLng);
+    google.maps.event.addListener(map, 'rightclick', delLatLng);
 };
+
+function newPoly(path){
+    if(path){
+        polyOptions.path=path;
+    }
+    poly = new google.maps.Polyline(polyOptions);
+    poly.setMap(map);
+}
+
+function addLatLng(event) {
+    var path = poly.getPath();
+    path.push(event.latLng);
+}
+
+function delLatLng(event) {
+    var path = poly.getPath();
+    var listCoord=path.getArray();
+    var countCoord=listCoord.length;
+    var flightPlanCoordinates=Array();
+    for (i = 0; i < (countCoord-1); i++) {
+        flightPlanCoordinates[i]=new google.maps.LatLng(listCoord[i].ob, listCoord[i].pb);
+    }
+    poly.setPath(flightPlanCoordinates);
+}
+
+function coordLatLng(event) {
+    var path = poly.getPath();
+    console.table(path.getArray());
+}
