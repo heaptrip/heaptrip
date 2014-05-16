0647f39cc7d6160735969300b1baad1d62f5d07e
Evgeny Konovalov <konovalov.inbox@gmail.com>
2014-05-16 10:36:02 +0400

    update ckeditor to Version 4.4.0 Standard Package

--- heaptrip/src/main/webapp/js/lib/ckeditor/config.js
+++ heaptrip/src/main/webapp/js/lib/ckeditor/config.js
@@ -5,13 +5,56 @@
 
 
 CKEDITOR.editorConfig = function (config) {
-    //config.language = 'fr';
-    config.uiColor = '#FFFFFF';
-   // config.height = '100%';
-    config.extraPlugins = 'fileUpload';
+    // Define changes to default configuration here.
+    // For complete reference see:
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+    config.extraPlugins = 'fileUpload,autogrow';
+    config.autoGrow_onStartup = true;
+
+    // The toolbar groups arrangement, optimized for two toolbar rows.
     config.toolbarGroups = [
-        { name: 'document', items: [ 'Source' ] },
-        { name: 'fileUpload', items: [ 'FileUpload','Image' ] }
-    ]
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
+        { name: 'links' },
+        { name: 'insert' },
+        { name: 'forms' },
+        { name: 'tools' },
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+        { name: 'others' },
+        '/',
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+        { name: 'styles' },
+        { name: 'colors' },
+        { name: 'about' }
+    ];
+
+    // Remove some buttons provided by the standard plugins, which are
+    // not needed in the Standard(s) toolbar.
+    config.removeButtons = 'Underline,Subscript,Superscript';
+
+    // Set the most common block elements.
+    config.format_tags = 'p;h1;h2;h3;pre';
+
+    // Simplify the dialog windows.
+    config.removeDialogTabs = 'image:advanced;link:advanced';
 };
+
+
+/*
+
+ CKEDITOR.editorConfig = function (config) {
+ //config.language = 'fr';
+ config.uiColor = '#FFFFFF';
+ // config.height = '100%';
+ config.extraPlugins = 'fileUpload';
+ config.toolbarGroups = [
+ { name: 'document', items: [ 'Source' ] },
+ { name: 'fileUpload', items: [ 'FileUpload', 'Image' ] }
+ ];
+ //config.extraPlugins = 'fileUpload,autogrow';
+ //config.autoGrow_onStartup = true;
+ };
+ */
 
