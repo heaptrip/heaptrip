f65dac1238c4cc9cb202c26214adadd596ec8d54
Evgeny Konovalov <konovalov.inbox@gmail.com>
2014-06-16 18:44:02 +0400

    fix getCurrentRequestIP

--- heaptrip/src/main/java/com/heaptrip/service/system/RequestScopeServiceImpl.java
+++ heaptrip/src/main/java/com/heaptrip/service/system/RequestScopeServiceImpl.java
@@ -1,84 +1,83 @@
 package com.heaptrip.service.system;
 
-import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
-
-import com.heaptrip.domain.service.rating.RatingService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-
 import com.heaptrip.domain.entity.account.user.User;
 import com.heaptrip.domain.service.system.ErrorService;
 import com.heaptrip.domain.service.system.LocaleService;
 import com.heaptrip.domain.service.system.RequestScopeService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
 
 @Service("requestScopeService")
 public class RequestScopeServiceImpl implements RequestScopeService {
 
-	@Autowired(required = false)
-	private HttpServletRequest request;
+    @Autowired(required = false)
+    private HttpServletRequest request;
 
-	@Autowired
-	private LocaleService localeService;
+    @Autowired
+    private LocaleService localeService;
 
-	@Autowired
-	private ErrorService errorService;
+    @Autowired
+    private ErrorService errorService;
 
-	@Override
-	public User getCurrentUser() {
-		User result = null;
-		try {
-			result = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		} catch (Throwable e) {
-		}
-		return result;
-	}
+    @Override
+    public User getCurrentUser() {
+        User result = null;
+        try {
+            result = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (Throwable e) {
+        }
+        return result;
+    }
 
-	@Override
-	public String getCurrentRequestUrl(boolean isWithParameters) {
+    @Override
+    public String getCurrentRequestUrl(boolean isWithParameters) {
 
-		String url = null;
+        String url = null;
 
-		if (request != null) {
-			url = getCurrentContextPath() + request.getServletPath();
-			if (isWithParameters) {
-				url = "?" + request.getQueryString();
-			}
-		}
+        if (request != null) {
+            url = getCurrentContextPath() + request.getServletPath();
+            if (isWithParameters) {
+                url = "?" + request.getQueryString();
+            }
+        }
 
-		return url;
-	}
+        return url;
+    }
 
-	@Override
-	public String getCurrentContextPath() {
-		return (request != null) ? request.getScheme() + "://" + request.getServerName() + ":"
-				+ request.getServerPort() + request.getContextPath() : "http://apptest.heaptrip.com";
-	}
+    @Override
+    public String getCurrentContextPath() {
+        return (request != null) ? request.getScheme() + "://" + request.getServerName() + ":"
+                + request.getServerPort() + request.getContextPath() : "http://apptest.heaptrip.com";
+    }
 
-	@Override
-	public ErrorService getErrorServise() {
-		return errorService;
-	}
+    @Override
+    public ErrorService getErrorServise() {
+        return errorService;
+    }
 
-	@Override
-	public HttpServletRequest getCurrentRequest() {
-		return request;
-	}
+    @Override
+    public HttpServletRequest getCurrentRequest() {
+        return request;
+    }
 
-	@Override
-	public Locale getCurrentLocale() {
-		return localeService.getCurrentLocale();
-	}
+    @Override
+    public Locale getCurrentLocale() {
+        return localeService.getCurrentLocale();
+    }
 
-	@Override
-	public String getMessage(String key) {
-		return localeService.getMessage(key);
-	}
+    @Override
+    public String getMessage(String key) {
+        return localeService.getMessage(key);
+    }
 
-	@Override
-	public String getCurrentRequestIP() {
-		return getCurrentRequest().getRemoteAddr();
-	}
+    @Override
+    public String getCurrentRequestIP() {
+        String realIP = getCurrentRequest().getHeader("X-Real-IP");
+        return StringUtils.isEmpty(realIP) ? getCurrentRequest().getRemoteAddr() : realIP;
+    }
 }
